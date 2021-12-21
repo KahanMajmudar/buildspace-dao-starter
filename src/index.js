216b3308ac888ffe765e6d933b5d3bbf9b2157ff
KahanMajmudar <kahan.m.btechi16@ahduni.edu.in>
2021-12-21 21:22:52 +0530

    Section 1 done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App.jsx'
+
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
+
+const supportedChainIDs = [4]
+
+const connectors = {
+	injected: {},
+}
 
 // Render the App component to the DOM
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+	<React.StrictMode>
+		<ThirdwebWeb3Provider
+			connectors={connectors}
+			supportedChainIDs={supportedChainIDs}
+		>
+			<div className="landing">
+				<App />
+			</div>
+		</ThirdwebWeb3Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
